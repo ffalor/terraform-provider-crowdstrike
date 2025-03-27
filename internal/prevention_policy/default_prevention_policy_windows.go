26764647ee31ccb32b1cf58d9e8201fb96ec0032
ffalor <35144141+ffalor@users.noreply.github.com>
2025-03-27 12:59:06 -0500

    wip

--- internal/prevention_policy/default_prevention_policy_windows.go
+++ internal/prevention_policy/default_prevention_policy_windows.go
@@ -1,16 +1,14 @@
-package defaultpreventionpolicywindows
+package preventionpolicy
 
 import (
 	"context"
 	"fmt"
 
 	"github.com/crowdstrike/gofalcon/falcon/client"
-	"github.com/crowdstrike/terraform-provider-crowdstrike/internal/scopes"
+	"github.com/crowdstrike/terraform-provider-crowdstrike/internal/utils"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 var (
@@ -18,11 +16,6 @@ var (
 	_ resource.ResourceWithConfigure      = &defaultPreventionPolicyWindowsResource{}
 	_ resource.ResourceWithImportState    = &defaultPreventionPolicyWindowsResource{}
 	_ resource.ResourceWithValidateConfig = &defaultPreventionPolicyWindowsResource{}
-)
-
-var (
-  resourceMarkdownDescription string         = ""
-  requiredScopes              []scopes.Scope = []scopes.Scope{}
 )
 
 func NewDefaultPreventionPolicyWindowsResource() resource.Resource {
@@ -34,7 +27,69 @@ type defaultPreventionPolicyWindowsResource struct {
 }
 
 type defaultPreventionPolicyWindowsResourceModel struct {
-  // TODO: Define resource model
+	ID          types.String `tfsdk:"id"`
+	LastUpdated types.String `tfsdk:"last_updated"`
+
+	Description                               types.String       `tfsdk:"description"`
+	RuleGroups                                types.Set          `tfsdk:"ioa_rule_groups"`
+	CloudAntiMalwareForMicrosoftOfficeFiles   *mlSlider          `tfsdk:"cloud_anti_malware_microsoft_office_files"`
+	ExtendedUserModeDataSlider                *detectionMlSlider `tfsdk:"extended_user_mode_data"`
+	CloudAntiMalware                          *mlSlider          `tfsdk:"cloud_anti_malware"`
+	AdwarePUP                                 *mlSlider          `tfsdk:"adware_and_pup"`
+	OnSensorMLSlider                          *mlSlider          `tfsdk:"sensor_anti_malware"`
+	OnSensorMLSliderForSensorEndUserScans     *mlSlider          `tfsdk:"sensor_anti_malware_user_initiated"`
+	OnSensorMLSliderForCloudEndUserScans      *mlSlider          `tfsdk:"cloud_anti_malware_user_initiated"`
+	AdditionalUserModeData                    types.Bool         `tfsdk:"additional_user_mode_data"`
+	EndUserNotifications                      types.Bool         `tfsdk:"notify_end_users"`
+	UnknownDetectionRelatedExecutables        types.Bool         `tfsdk:"upload_unknown_detection_related_executables"`
+	UnknownExecutables                        types.Bool         `tfsdk:"upload_unknown_executables"`
+	SensorTamperingProtection                 types.Bool         `tfsdk:"sensor_tampering_protection"`
+	InterpreterProtection                     types.Bool         `tfsdk:"interpreter_only"`
+	EngineProtectionV2                        types.Bool         `tfsdk:"engine_full_visibility"`
+	ScriptBasedExecutionMonitoring            types.Bool         `tfsdk:"script_based_execution_monitoring"`
+	HTTPDetections                            types.Bool         `tfsdk:"http_detections"`
+	RedactHTTPDetectionDetails                types.Bool         `tfsdk:"redact_http_detection_details"`
+	HardwareEnhancedExploitDetection          types.Bool         `tfsdk:"hardware_enhanced_exploit_detection"`
+	EnhancedExploitationVisibility            types.Bool         `tfsdk:"enhanced_exploitation_visibility"`
+	DLLLoadVisibility                         types.Bool         `tfsdk:"enhanced_dll_load_visibility"`
+	MemoryScan                                types.Bool         `tfsdk:"memory_scanning"`
+	CPUMemoryScan                             types.Bool         `tfsdk:"memory_scanning_scan_with_cpu"`
+	FirmwareAnalysisExtraction                types.Bool         `tfsdk:"bios_deep_visibility"`
+	MLLargeFileHandling                       types.Bool         `tfsdk:"enhanced_ml_for_larger_files"`
+	USBInsertionTriggeredScan                 types.Bool         `tfsdk:"usb_insertion_triggered_scan"`
+	DetectOnWrite                             types.Bool         `tfsdk:"detect_on_write"`
+	QuarantineOnWrite                         types.Bool         `tfsdk:"quarantine_on_write"`
+	OnWriteScriptFileVisibility               types.Bool         `tfsdk:"on_write_script_file_visibility"`
+	NextGenAV                                 types.Bool         `tfsdk:"quarantine_and_security_center_registration"`
+	NextGenAVQuarantineOnRemovableMedia       types.Bool         `tfsdk:"quarantine_on_removable_media"`
+	MicrosoftOfficeFileSuspiciousMacroRemoval types.Bool         `tfsdk:"microsoft_office_file_suspicious_macro_removal"`
+	CustomBlacklisting                        types.Bool         `tfsdk:"custom_blocking"`
+	PreventSuspiciousProcesses                types.Bool         `tfsdk:"prevent_suspicious_processes"`
+	SuspiciousRegistryOperations              types.Bool         `tfsdk:"suspicious_registry_operations"`
+	MaliciousPowershell                       types.Bool         `tfsdk:"suspicious_scripts_and_commands"`
+	IntelPrevention                           types.Bool         `tfsdk:"intelligence_sourced_threats"`
+	SuspiciousKernelDrivers                   types.Bool         `tfsdk:"driver_load_prevention"`
+	VulnerableDriverProtection                types.Bool         `tfsdk:"vulnerable_driver_protection"`
+	ForceASLR                                 types.Bool         `tfsdk:"force_aslr"`
+	ForceDEP                                  types.Bool         `tfsdk:"force_dep"`
+	HeapSprayPreallocation                    types.Bool         `tfsdk:"heap_spray_preallocation"`
+	NullPageAllocation                        types.Bool         `tfsdk:"null_page_allocation"`
+	SEHOverwriteProtection                    types.Bool         `tfsdk:"seh_overwrite_protection"`
+	BackupDeletion                            types.Bool         `tfsdk:"backup_deletion"`
+	Cryptowall                                types.Bool         `tfsdk:"cryptowall"`
+	FileEncryption                            types.Bool         `tfsdk:"file_encryption"`
+	Locky                                     types.Bool         `tfsdk:"locky"`
+	FileSystemAccess                          types.Bool         `tfsdk:"file_system_access"`
+	VolumeShadowCopyAudit                     types.Bool         `tfsdk:"volume_shadow_copy_audit"`
+	VolumeShadowCopyProtect                   types.Bool         `tfsdk:"volume_shadow_copy_protect"`
+	ApplicationExploitationActivity           types.Bool         `tfsdk:"application_exploitation_activity"`
+	ChopperWebshell                           types.Bool         `tfsdk:"chopper_webshell"`
+	DriveByDownload                           types.Bool         `tfsdk:"drive_by_download"`
+	ProcessHollowing                          types.Bool         `tfsdk:"code_injection"`
+	JavaScriptViaRundll32                     types.Bool         `tfsdk:"javascript_via_rundll32"`
+	WindowsLogonBypassStickyKeys              types.Bool         `tfsdk:"windows_logon_bypass_sticky_keys"`
+	CredentialDumping                         types.Bool         `tfsdk:"credential_dumping"`
+	AutomatedRemediation                      types.Bool         `tfsdk:"advanced_remediation"`
 }
 
 func (r *defaultPreventionPolicyWindowsResource) Configure(
@@ -76,25 +131,7 @@ func (r *defaultPreventionPolicyWindowsResource) Schema(
 	_ resource.SchemaRequest,
 	resp *resource.SchemaResponse,
 ) {
-	resp.Schema = schema.Schema{
-		MarkdownDescription: fmt.Sprintf(
-		  resourceMarkdownDescription,	
-			scopes.GenerateScopeDescription(requiredScopes),
-		),
-		Attributes: map[string]schema.Attribute{
-			"id": schema.StringAttribute{
-				Computed:    true,
-				Description: "Identifier for the Default Prevention Policy Windows.",
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.UseStateForUnknown(),
-				},
-			},
-			"last_updated": schema.StringAttribute{
-				Computed:    true,
-				Description: "Timestamp of the last Terraform update of the resource.",
-			},	
-		},
-	}
+	resp.Schema = generateWindowsSchema(true)
 }
 
 func (r *defaultPreventionPolicyWindowsResource) Create(
@@ -102,7 +139,7 @@ func (r *defaultPreventionPolicyWindowsResource) Create(
 	req resource.CreateRequest,
 	resp *resource.CreateResponse,
 ) {
-  var newState defaultPreventionPolicyWindowsResourceModel
+	var newState defaultPreventionPolicyWindowsResourceModel
 
 	var plan defaultPreventionPolicyWindowsResourceModel
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
@@ -118,7 +155,7 @@ func (r *defaultPreventionPolicyWindowsResource) Read(
 	req resource.ReadRequest,
 	resp *resource.ReadResponse,
 ) {
-  var newState defaultPreventionPolicyWindowsResourceModel
+	var newState defaultPreventionPolicyWindowsResourceModel
 
 	var state defaultPreventionPolicyWindowsResourceModel
 	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
@@ -134,7 +171,7 @@ func (r *defaultPreventionPolicyWindowsResource) Update(
 	req resource.UpdateRequest,
 	resp *resource.UpdateResponse,
 ) {
-  var newState defaultPreventionPolicyWindowsResourceModel
+	var newState defaultPreventionPolicyWindowsResourceModel
 
 	var plan defaultPreventionPolicyWindowsResourceModel
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
@@ -146,7 +183,8 @@ func (r *defaultPreventionPolicyWindowsResource) Delete(
 	ctx context.Context,
 	req resource.DeleteRequest,
 	resp *resource.DeleteResponse,
-) {}
+) {
+}
 
 func (r *defaultPreventionPolicyWindowsResource) ImportState(
 	ctx context.Context,
@@ -163,5 +201,190 @@ func (r *defaultPreventionPolicyWindowsResource) ValidateConfig(
 ) {
 	var config defaultPreventionPolicyWindowsResourceModel
 	resp.Diagnostics.Append(req.Config.Get(ctx, &config)...)
-}
+	if resp.Diagnostics.HasError() {
+		return
+	}
 
+	resp.Diagnostics.Append(utils.ValidateEmptyIDs(ctx, config.RuleGroups, "ioa_rule_groups")...)
+
+	resp.Diagnostics.Append(
+		validateRequiredAttribute(
+			config.ProcessHollowing.ValueBool(),
+			config.AdditionalUserModeData.ValueBool(),
+			"code_injection",
+			"additional_user_mode_data",
+		)...)
+
+	resp.Diagnostics.Append(
+		validateRequiredAttribute(
+			config.ForceASLR.ValueBool(),
+			config.AdditionalUserModeData.ValueBool(),
+			"force_aslr",
+			"additional_user_mode_data",
+		)...)
+
+	resp.Diagnostics.Append(
+		validateRequiredAttribute(
+			config.ForceDEP.ValueBool(),
+			config.AdditionalUserModeData.ValueBool(),
+			"force_dep",
+			"additional_user_mode_data",
+		)...)
+
+	resp.Diagnostics.Append(
+		validateRequiredAttribute(
+			config.HeapSprayPreallocation.ValueBool(),
+			config.AdditionalUserModeData.ValueBool(),
+			"heap_spray_preallocation",
+			"additional_user_mode_data",
+		)...)
+
+	resp.Diagnostics.Append(
+		validateRequiredAttribute(
+			config.NullPageAllocation.ValueBool(),
+			config.AdditionalUserModeData.ValueBool(),
+			"null_page_allocation",
+			"additional_user_mode_data",
+		)...)
+
+	resp.Diagnostics.Append(
+		validateRequiredAttribute(
+			config.CredentialDumping.ValueBool(),
+			config.AdditionalUserModeData.ValueBool(),
+			"credential_dumping",
+			"additional_user_mode_data",
+		)...)
+
+	resp.Diagnostics.Append(
+		validateRequiredAttribute(
+			config.SEHOverwriteProtection.ValueBool(),
+			config.AdditionalUserModeData.ValueBool(),
+			"seh_overwrite_protection",
+			"additional_user_mode_data",
+		)...)
+
+	resp.Diagnostics.Append(
+		validateRequiredAttribute(
+			config.EngineProtectionV2.ValueBool(),
+			config.InterpreterProtection.ValueBool(),
+			"engine_full_visibility",
+			"interpreter_only",
+		)...)
+
+	resp.Diagnostics.Append(
+		validateRequiredAttribute(
+			config.CPUMemoryScan.ValueBool(),
+			config.MemoryScan.ValueBool(),
+			"memory_scanning_scan_with_cpu",
+			"memory_scanning",
+		)...)
+
+	resp.Diagnostics.Append(
+		validateRequiredAttribute(
+			config.VolumeShadowCopyProtect.ValueBool(),
+			config.VolumeShadowCopyAudit.ValueBool(),
+			"volume_shadow_copy_protect",
+			"volume_shadow_copy_audit",
+		)...)
+
+	resp.Diagnostics.Append(
+		validateRequiredAttribute(
+			config.VulnerableDriverProtection.ValueBool(),
+			config.SuspiciousKernelDrivers.ValueBool(),
+			"vulnerable_driver_protection",
+			"driver_load_prevention",
+		)...)
+
+	resp.Diagnostics.Append(
+		validateRequiredAttribute(
+			config.QuarantineOnWrite.ValueBool(),
+			(config.NextGenAV.ValueBool() && config.DetectOnWrite.ValueBool()),
+			"quarantine_on_write",
+			"quarantine_and_security_center_registration and detect_on_write",
+		)...)
+
+	resp.Diagnostics.Append(
+		validateRequiredAttribute(
+			config.ScriptBasedExecutionMonitoring.ValueBool(),
+			config.NextGenAV.ValueBool(),
+			"script_based_execution_monitoring",
+			"quarantine_and_security_center_registration",
+		)...)
+
+	resp.Diagnostics.Append(
+		validateRequiredAttribute(
+			config.MaliciousPowershell.ValueBool(),
+			(config.InterpreterProtection.ValueBool() || config.ScriptBasedExecutionMonitoring.ValueBool()),
+			"suspicious_scripts_and_commands",
+			"interpreter_only or script_based_execution_monitoring",
+		)...)
+
+	if config.USBInsertionTriggeredScan.ValueBool() {
+		sensorDetection := "DISABLED"
+		cloudDetection := "DISABLED"
+
+		if config.OnSensorMLSliderForCloudEndUserScans != nil {
+			sensorDetection = config.OnSensorMLSliderForCloudEndUserScans.Detection.ValueString()
+		}
+
+		if config.OnSensorMLSliderForCloudEndUserScans != nil {
+			cloudDetection = config.OnSensorMLSliderForCloudEndUserScans.Detection.ValueString()
+		}
+
+		if sensorDetection == "DISABLED" && cloudDetection == "DISABLED" {
+			resp.Diagnostics.AddAttributeError(
+				path.Root("usb_insertion_triggered_scan"),
+				"requirements not met to enable usb_insertion_triggered_scan",
+				"Either sensor_anti_malware_user_initiated or cloud_anti_malware_user_initiated detection must be a level higher than DISABLED to enable usb_insertion_triggered_scan",
+			)
+		}
+	}
+
+	if config.CloudAntiMalwareForMicrosoftOfficeFiles != nil {
+		resp.Diagnostics.Append(
+			validateMlSlider(
+				"cloud_anti_malware_microsoft_office_files",
+				*config.CloudAntiMalwareForMicrosoftOfficeFiles,
+			)...)
+	}
+
+	if config.CloudAntiMalware != nil {
+		resp.Diagnostics.Append(
+			validateMlSlider(
+				"cloud_anti_malware",
+				*config.CloudAntiMalware,
+			)...)
+	}
+
+	if config.AdwarePUP != nil {
+		resp.Diagnostics.Append(
+			validateMlSlider(
+				"adware_and_pup",
+				*config.AdwarePUP,
+			)...)
+	}
+
+	if config.OnSensorMLSlider != nil {
+		resp.Diagnostics.Append(
+			validateMlSlider(
+				"sensor_anti_malware",
+				*config.OnSensorMLSlider,
+			)...)
+	}
+
+	if config.OnSensorMLSliderForSensorEndUserScans != nil {
+		resp.Diagnostics.Append(
+			validateMlSlider(
+				"sensor_anti_malware_user_initiated",
+				*config.OnSensorMLSliderForSensorEndUserScans,
+			)...)
+	}
+
+	if config.OnSensorMLSliderForCloudEndUserScans != nil {
+		resp.Diagnostics.Append(
+			validateMlSlider(
+				"cloud_anti_malware_user_initiated",
+				*config.OnSensorMLSliderForCloudEndUserScans,
+			)...)
+	}
+}
